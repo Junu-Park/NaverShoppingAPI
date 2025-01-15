9db0497d50ba7bf25784e43733716cca009f4805
Park Junwoo <ays99111@naver.com>
2025-01-15 20:41:00 +0900

    [feat] SearchResult 뷰에서 사용할 데이터 모델 생성

---
+++ NaverShoppingAPI/Model/SearchResult.swift
@@ -0,0 +1,36 @@
+//
+//  SearchResult.swift
+//  NaverShoppingAPI
+//
+//  Created by 박준우 on 1/15/25.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    var totalCount: Int
+    var start: Int
+    var display: Int
+    var items: [SearchResultItem]
+    
+    enum CodingKeys: String, CodingKey {
+        case totalCount = "total"
+        case start
+        case display
+        case items
+    }
+}
+
+struct SearchResultItem: Decodable {
+    var title: String
+    var mallName: String
+    var image: String
+    var lowPrice: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case mallName
+        case image
+        case lowPrice = "lprice"
+    }
+}
