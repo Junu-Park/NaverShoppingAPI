970cb0d47dd15758f8f21a5d90dd16a0f1ce3919
Park Junwoo <ays99111@naver.com>
2025-01-15 16:35:00 +0900

    [feat] SearchResult VC 생성 및 View 폴더 하위 파일들 폴더링

---
+++ NaverShoppingAPI/View/SearchResult/SearchResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResultViewController.swift
+//  NaverShoppingAPI
+//
+//  Created by 박준우 on 1/15/25.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
