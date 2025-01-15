73d450be5fc288989f30e587af4c8f6b3f7003b8
Park Junwoo <ays99111@naver.com>
2025-01-15 23:33:54 +0900

    [feat] API 통신은 위한 URL enum 생성

---
+++ NaverShoppingAPI/API/APIURL.swift
@@ -0,0 +1,11 @@
+//
+//  APIURL.swift
+//  NaverShoppingAPI
+//
+//  Created by 박준우 on 1/15/25.
+//
+
+import Foundation
+enum APIURL {
+    static let naverShoppingBaseURL: String = "https://openapi.naver.com/v1/search/shop.json?"
+}
