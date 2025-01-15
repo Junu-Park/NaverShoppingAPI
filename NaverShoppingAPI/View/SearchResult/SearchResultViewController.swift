4ec3c30b2007e08c27c2d61f21085fcc8f2f0199
Park Junwoo <ays99111@naver.com>
2025-01-15 21:52:12 +0900

    [feat] SearchResult 뷰 컬렉션 뷰 적용

--- NaverShoppingAPI/View/SearchResult/SearchResultViewController.swift
+++ NaverShoppingAPI/View/SearchResult/SearchResultViewController.swift
@@ -19,20 +19,30 @@ class SearchResultViewController: CustomViewController {
         return lb
     }()
     
+    lazy var resultCollectionView: SearchResultCollectionView = SearchResultCollectionView(superView: view)
+    
     var data: SearchResult?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        connectionCollectionView()
     }
     
     override func configureHierarchy() {
         view.addSubview(resultCountLabel)
+        view.addSubview(resultCollectionView)
     }
     
     override func configureLayout() {
         resultCountLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
             make.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        resultCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(resultCountLabel.snp.bottom).offset(8)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
 }
@@ -45,5 +55,23 @@ extension SearchResultViewController {
         } else {
             return "0 개의 검색 결과"
         }
+    }
+}
+
+extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func connectionCollectionView() {
+        resultCollectionView.delegate = self
+        resultCollectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data?.items.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = resultCollectionView.dequeueReusableCell(withReuseIdentifier: SearchResultCollectionViewCell.id, for: indexPath) as! SearchResultCollectionViewCell
+        
+        return cell
     }
 }
