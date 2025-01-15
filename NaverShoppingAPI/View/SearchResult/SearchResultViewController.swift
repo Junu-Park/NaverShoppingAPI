a6e9951745835cc62374849a6850dd653da02a37
Park Junwoo <ays99111@naver.com>
2025-01-15 21:38:27 +0900

    [feat] SearchResult 뷰 검색 결과 개수 표시하는 레이블 추가

--- NaverShoppingAPI/View/SearchResult/SearchResultViewController.swift
+++ NaverShoppingAPI/View/SearchResult/SearchResultViewController.swift
@@ -7,10 +7,43 @@
 
 import UIKit
 
-class SearchResultViewController: UIViewController {
+import SnapKit
 
+class SearchResultViewController: CustomViewController {
+
+    lazy var resultCountLabel: UILabel = {
+        let lb: UILabel = UILabel()
+        lb.textColor = UIColor.systemGreen
+        lb.font = UIFont.boldSystemFont(ofSize: 13)
+        lb.text = getResultCountText()
+        return lb
+    }()
+    
+    var data: SearchResult?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(resultCountLabel)
+    }
+    
+    override func configureLayout() {
+        resultCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
+    }
+}
+
+extension SearchResultViewController {
+    func getResultCountText() -> String {
         
+        if let data {
+            return "\(data.totalCount.formatted()) 개의 검색 결과"
+        } else {
+            return "0 개의 검색 결과"
+        }
     }
 }
