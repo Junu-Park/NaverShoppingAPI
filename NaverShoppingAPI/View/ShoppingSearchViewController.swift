44bd977576b80fba210a69a115cdc1d67372ac69
Park Junwoo <ays99111@naver.com>
2025-01-15 16:28:21 +0900

    [feat] ShoppingSearch VC 생성

---
+++ NaverShoppingAPI/View/ShoppingSearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingSearchViewController.swift
+//  NaverShoppingAPI
+//
+//  Created by 박준우 on 1/15/25.
+//
+
+import UIKit
+
+class ShoppingSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
