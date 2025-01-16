8bf2868ea795c3235ea74c0bc69f5f1c4f4e44b3
Park Junwoo <ays99111@naver.com>
2025-01-16 23:22:14 +0900

    [feat] API response 모델 가독성을 위한 변수명 수정

--- NaverShoppingAPI/Model/SearchResult.swift
+++ NaverShoppingAPI/Model/SearchResult.swift
@@ -9,26 +9,26 @@ import Foundation
 
 struct SearchResult: Decodable {
     var totalCount: Int
-    var start: Int
+    var pageOffset: Int
     var display: Int
     var items: [SearchResultItem]
     
     enum CodingKeys: String, CodingKey {
         case totalCount = "total"
-        case start
+        case pageOffset = "start"
         case display
         case items
     }
 }
 
 struct SearchResultItem: Decodable {
-    var title: String
+    var itemName: String
     var mallName: String
     var image: String
     var lowPrice: String
     
     enum CodingKeys: String, CodingKey {
-        case title
+        case itemName = "title"
         case mallName
         case image
         case lowPrice = "lprice"
