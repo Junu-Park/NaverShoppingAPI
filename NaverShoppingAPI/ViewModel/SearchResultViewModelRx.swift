075ed81d626b800ae91fc8ede9856d95f16a6998
Park Junwoo <ays99111@naver.com>
2025-02-25 23:57:45 +0900

    [feat] 검색 결과 화면 RxSwift를 활용한 ViewModel 생성

---
+++ NaverShoppingAPI/ViewModel/SearchResultViewModelRx.swift
@@ -0,0 +1,27 @@
+//
+//  SearchResultViewModelRx.swift
+//  NaverShoppingAPI
+//
+//  Created by 박준우 on 2/25/25.
+//
+
+import Foundation
+
+import RxCocoa
+import RxSwift
+
+final class SearchResultViewModelRx {
+    struct Input {
+        
+    }
+    struct Output {
+        
+    }
+    
+    private let disposeBag: DisposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+}
