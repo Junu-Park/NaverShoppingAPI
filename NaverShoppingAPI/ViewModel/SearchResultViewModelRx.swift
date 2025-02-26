72c9a331bea3fcb252413396851a91fc6dcc8c8e
Park Junwoo <ays99111@naver.com>
2025-02-26 20:03:16 +0900

    [feat] 검색 결과 화면 RxSwift를 활용한 ViewModel 구현

--- NaverShoppingAPI/ViewModel/SearchResultViewModelRx.swift
+++ NaverShoppingAPI/ViewModel/SearchResultViewModelRx.swift
@@ -12,16 +12,56 @@ import RxSwift
 
 final class SearchResultViewModelRx {
     struct Input {
-        
+        let searchText: BehaviorRelay<String> = BehaviorRelay(value: "")
+        var searchTextWithSort: PublishRelay<SortOption> = PublishRelay()
     }
     struct Output {
-        
+        let searchResult: Driver<(SortOption, [SearchResultItem])>
     }
     
     private let disposeBag: DisposeBag = DisposeBag()
     
     func transform(input: Input) -> Output {
         
-        return Output()
+        let searchResult = PublishSubject<(SortOption, [SearchResultItem])>()
+        
+        input.searchText
+            .flatMap{ NetworkManager.shared.naverShoppingSearchRequestWithRx(searchText: $0) }
+            .subscribe { value in
+                switch value {
+                case .success(let result):
+                    searchResult.onNext((.accuracy,result.items))
+                case .failure(let error):
+                    searchResult.onNext((.accuracy,[]))
+                }
+            } onError: { error in
+                print("input.searchText \(error)")
+            } onCompleted: {
+                print("input.searchText onCompleted")
+            } onDisposed: {
+                print("input.searchText onDisposed")
+            }
+            .disposed(by: self.disposeBag)
+        
+        input.searchTextWithSort
+            .withLatestFrom(input.searchText) { sort, text in
+                return (sort, text)
+            }
+            .subscribe { value in
+                NetworkManager.shared.resetSharedDataWithChangeSort(sort: value.0)
+                NetworkManager.shared.naverShoppingSearchRequestWithRx(searchText: value.1) { response in
+                    searchResult.onNext((value.0 ,response.items))
+                }
+            } onError: { error in
+                print("input.searchTextWithSort \(error)")
+            } onCompleted: {
+                print("input.searchTextWithSort onCompleted")
+            } onDisposed: {
+                print("input.searchTextWithSort onDisposed")
+            }
+            .disposed(by: self.disposeBag)
+
+        
+        return Output(searchResult: searchResult.asDriver(onErrorJustReturn: (SortOption.accuracy, [])))
     }
 }
